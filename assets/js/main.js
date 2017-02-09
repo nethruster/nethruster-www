bbb4ddaa5455dcf965847906a8c1ee9baa5a7fa7
Guillem Arias <gariasfa7@alumnes.ub.edu>
2017-02-09 23:13:20 +0100

    Added dynamic header middle titles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
 'use strict'
 
+const homeTitle = 'Welcome to Nethruster';
+const teamTitle = 'This is us';
+const statusTitle = 'Our workflow';
+const contactTitle = 'Get in touch';
+
+const teamDesc = 'Learn about Nethruster\'s core';
+const statusDesc = 'Explore and keep track of our work';
+const contactDesc = 'Have any questions? Wanna say hello? Send us something!';
+
 function init(){
     setTab('home');
 }
@@ -11,11 +20,48 @@ function fetchBlogData(){
 function setTab(tab, position) {
     let activeElements = document.getElementsByClassName("active");
     let tabSlider = document.getElementById("tabs-slider");
+    let scrt = document.getElementById("scrt");
+    let title = document.getElementById("title");
+    let desc = document.getElementById("desc");
+
+    // Remove the active class from the current active element
     if(activeElements[0]){
         activeElements[0].classList.toggle('active');
     }
-
+    // Add the active class to the selected element 
     document.getElementById(`${tab}-tab`).classList.toggle('active');
+
+    switch (tab) {
+        case 'home':
+            title.innerHTML = homeTitle;
+            desc.innerHTML = '';
+            desc.style.display = 'none';
+            scrt.style.display = 'block';
+            break;
+        case 'team':
+            title.innerHTML = teamTitle;
+            desc.innerHTML = teamDesc;
+            desc.style.display = 'block';
+            scrt.style.display = 'none';
+            break;
+        case 'status':
+            title.innerHTML = statusTitle;
+            desc.innerHTML = statusDesc;
+            desc.style.display = 'block';
+            scrt.style.display = 'none';
+            break;
+        case 'contact':
+            title.innerHTML = contactTitle;
+            desc.innerHTML = contactDesc;
+            desc.style.display = 'block';
+            scrt.style.display = 'none';
+            break;
+        default: 
+             title.innerHTML = homeTitle;
+             break;
+    }
+
+    // Move slider to the correct position
     tabSlider.style.left = `${position}%`;
 
 }
