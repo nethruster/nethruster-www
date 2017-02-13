fc8415d7d3f46979cfa6a57336f18cf6c46d515e
Guillem Arias <gariasfa7@alumnes.ub.edu>
2017-02-13 14:24:17 +0100

    Implemented routing and fixed a few things

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,16 +1,18 @@
 import "../scss/styles.scss";
 import TabHandler from "./partials/tab-handler";
 import TextScramble from "./partials/text-scramble";
+import handleRoute from "./partials/route-handler";
 
 function init() {
   var handler = new TabHandler("header-tab", "tabs-slider");
+  handleRoute(handler);
   handler.addListeners();
   var screableContainer = <HTMLElement>document.querySelector('.scramble-text');
-  var screamblePhrases = ['We design,', 'we code,', 'we create.'];
-  var screamble = new TextScramble(screableContainer, 1000, screamblePhrases);
-  screamble.start();
+  var scramblePhrases = ['We design,', 'we code,', 'we create.'];
+  var scramble = new TextScramble(screableContainer, 1000, scramblePhrases);
+  scramble.start();
 }
 
-window.addEventListener("load", init);
+init();
 
 
--- src/ts/partials/route-handler.ts
+++ src/ts/partials/route-handler.ts
@@ -1,3 +1,20 @@
-class RouteHandler {
-  
+import TabHandler from './tab-handler';
+
+function handleRoute(handler: TabHandler){
+  let param = /[^/]*$/.exec(location.pathname)[0];
+
+  if(param && param != null) {
+    let tabEl = document.getElementById(`${param}-tab`);
+    
+    if(tabEl) {
+      handler.setTab(tabEl);
+    } else {
+      console.log("The tab does not exist");
+    }
+  } else {
+    // We are at root path
+    handler.setTab(document.getElementById("home-tab"));
+  }
 }
+
+export default handleRoute;
