23cd9ac8c0d569cadb259ec9643493f5e16b7ae3
Guillem Arias <gariasfa7@alumnes.ub.edu>
2017-02-13 14:56:17 +0100

    Small refactor

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,11 +1,11 @@
 import "../scss/styles.scss";
 import TabHandler from "./partials/tab-handler";
 import TextScramble from "./partials/text-scramble";
-import handleRoute from "./partials/route-handler";
+import routeHandler from "./partials/route-handler";
 
 function init() {
   var handler = new TabHandler("header-tab", "tabs-slider");
-  handleRoute(handler);
+  routeHandler(handler);
   handler.addListeners();
   var screableContainer = <HTMLElement>document.querySelector('.scramble-text');
   var scramblePhrases = ['We design,', 'we code,', 'we create.'];
