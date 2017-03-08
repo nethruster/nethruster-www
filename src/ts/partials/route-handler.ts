5f24ae35e4e2a24cc35b79e7328256d8609a8f94
Guillem Arias <gariasfa7@alumnes.ub.edu>
2017-03-08 17:19:50 +0100

    Implemented not-found tab

--- src/ts/partials/route-handler.ts
+++ src/ts/partials/route-handler.ts
@@ -5,12 +5,7 @@ function routeHandler(handler: TabHandler){
 
   if(param && param != null) {
     let tabEl = document.getElementById(`${param}-tab`); /* Select tab section */
-    
-    if(tabEl) {
-      handler.setTab(tabEl);
-    } else {
-      /* TODO use a not found tab/element */
-    }
+    handler.setTab(tabEl);
   } else {
     /* In case no tab is specified, go home */
     handler.setTab(document.getElementById("home-tab"));
