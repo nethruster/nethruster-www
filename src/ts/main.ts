4cce3a0bdd1931f2c650767a0cba757eda68e475
Guillem Arias <gariasfa7@alumnes.ub.edu>
2017-03-08 15:58:03 +0100

    Added operational blog feed to the home tab

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -9,9 +9,30 @@ import autosize from "./partials/autosize";
 import "./partials/parallax";
 declare var Parallax: any;
 
+function populateFromFeed (posts) {
+  const entriesContainer = document.getElementById('blog-entries');
+  const entryClassNames = ['blog-entry', 'flex', 'flex-dc', 'flex-main-center'];
+  
+  posts.forEach((post, index) => {
+    let temp;
+    if(index <= 3 && posts.length != 0) {
+      temp = document.createElement('div');
+      let date = /\d\d\s\w+\s\d+/.exec(post.pubdate);
+      temp.classList.add(...entryClassNames);
+      temp.innerHTML = `<h4 class="entry-title"><a href="${post.link}" rel="noopener" target="_blank">${post.title}</a></h4><span class="entry-data"><span class="entry-author">${post["dc:creator"]}</span>&nbsp;&dash;&nbsp;<span class="entry-date">${date}</span></span>`;
+      entriesContainer.appendChild(temp);
+    } else {
+      temp = document.createElement('div');
+      temp.classList.add(...entryClassNames);
+      temp.innerHTML = `There are no posts here D:`;
+      entriesContainer.appendChild(temp);
+    }
+  })
+};
+
 function init() {
   try {
-    if (navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv 11/))){
+    if (navigator.appName == 'Microsoft Internet Explorer' || !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv 11/))){
       alert("In order to enjoy our website properly, we recommend using modern web browsers such as Google Chrome or Mozilla Firefox.");
       console.warn("In order to enjoy our website properly, we recommend using modern web browsers such as Google Chrome or Mozilla Firefox.");
     }
@@ -35,20 +56,22 @@ function init() {
     '600': 'Invalid recaptcha.'
   };
   var scramblePhrases = ['We design,', 'we code,', 'we create.'];
+  var blogUrl = "https://blog.gariasf.com/rss/"
+  
   // Init
   var tabHandler = new TabHandler("header-tab", "tabs-slider");
   tabHandler.addListeners();
   routeHandler(tabHandler);
   new CfHandler(cfMessages);
-  // var rssReader = new RssReader(blogUrl);
   new MdRipple();
   autosize(document.querySelector('textarea'));
+  new Parallax(document.getElementsByClassName('scene')[0]);
   var scrambleContainer = <HTMLElement>document.querySelector('.scramble-text');
   var scramble = new TextScramble(scrambleContainer, 1500, scramblePhrases);
-  new Parallax(document.getElementsByClassName('scene')[0]);
   scramble.start();
+  var rssReader = new RssReader(blogUrl);
+  rssReader.getFeedAsync(populateFromFeed);
   console.log("Humans worked on this project! https://nethruster.com/humans.txt");
-  
 }
 
 init();
